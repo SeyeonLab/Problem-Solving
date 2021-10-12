e4e494c0017d9a239a3c1a55f46ef12e7d35d103
Seyeon Yoon <seyeonlab@gmail.com>
2021-10-12 20:32:43 +0900

    Update Priority_Queue(Heap)

--- BaekJoon/Code/11286_절댓값힙.cpp
+++ BaekJoon/Code/11286_절댓값힙.cpp
@@ -1,20 +1,28 @@
 #include <iostream>
+#include <vector>
 #include <queue>
+#define P pair<int, int> 
 
 using namespace std;
 
+
 int main(void) {
     ios::sync_with_stdio(false);  cin.tie(NULL);  cout.tie(NULL);
-    priority_queue<int> pq;
+    priority_queue<P, vector<P>, greater<P>> pq;
     
     int N;  cin >> N;
     for (int i = 0; i < N; i++) {
         int x;  cin >> x;
-        if (x > 0)
-            pq.push(x);
-        else if (x == 0) {
+        
+        if (x > 0) {
+            pq.push({x, x});
+        }
+        else if (x < 0) {
+            pq.push({-x, x});
+        }
+        else {
             if (!pq.empty()) {
-                cout << pq.top() << '\n';
+                cout << pq.top().second << '\n';
                 pq.pop();
             }
             else
--- BaekJoon/Code/1927_최소힙.cpp
+++ BaekJoon/Code/1927_최소힙.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
+#include <vector>
 #include <queue>
 
 using namespace std;
 
 int main(void) {
     ios::sync_with_stdio(false);  cin.tie(NULL);  cout.tie(NULL);
-    priority_queue<int> pq;
+    priority_queue<int, vector<int>, greater<int>> pq;
     
     int N;  cin >> N;
     for (int i = 0; i < N; i++) {
