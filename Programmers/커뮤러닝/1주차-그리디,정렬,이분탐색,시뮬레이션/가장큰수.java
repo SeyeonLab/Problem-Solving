735a147f4b29f76e3183af33ef92bf37f8f891e2
dogfooter <yoon971224@gmail.com>
2022-02-21 12:36:47 +0900

    정렬

--- Programmers/커뮤러닝/1주차-그리디,정렬,이분탐색,시뮬레이션/가장큰수.java
+++ Programmers/커뮤러닝/1주차-그리디,정렬,이분탐색,시뮬레이션/가장큰수.java
@@ -2,13 +2,11 @@
     1. Java 언어를 사용한다 == Java가 제공하는 라이브러리를 사용
     2. 기본 라이브러리인 java.lang.*, java.util.*의 사용법을 숙지  */
 
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.Collections;
+import java.util.*;
 
 class Solution {
     public String solution(int[] numbers) {
-        
+
         // Java 8의 람다식을 활용한 풀이
         String answer = IntStream.of(numbers).mapToObj(String::valueOf).sorted((s1,s2) -> (s2+s1).compareTo(s1+s2)).collect(Collectors.joining());
         
