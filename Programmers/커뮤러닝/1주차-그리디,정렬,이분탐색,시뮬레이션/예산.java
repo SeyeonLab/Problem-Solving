35cee11ec9804d5ae74eab7979d9a27961746d4a
dogfooter <yoon971224@gmail.com>
2022-02-21 12:56:02 +0900

    이분탐색

--- Programmers/커뮤러닝/1주차-그리디,정렬,이분탐색,시뮬레이션/예산.java
+++ Programmers/커뮤러닝/1주차-그리디,정렬,이분탐색,시뮬레이션/예산.java
@@ -1,36 +1,40 @@
+/*  Tip. 좋은 프로그램을 만드는 방법
+    1. 당면한 문제를 해결한다
+    2. 좋은코드가 되도록 계속 리팩토링한다
+    3. 람다식을 활용할 수 있는 Stream을 응용한다  */
+
+import java.util.stream.*;
+
 class Solution { 
     public int solution(int[] budgets, int M) {
-        long answer = 0;
-        long sum = 0, low = 1, high = 0;
-        
-        for (int i = 0; i < budgets.length; ++i) {
-            sum += budgets[i];
-            high = Math.max(high, budgets[i]);
-        }
-        
-        if (sum <= M) {
-            answer = high;
-            return (int)answer;
-        }
-        
-        while (low <= high) {
-            long mid = low + (high - low) / 2;
+        int answer = 0;
+        int min = 0;
+        int max = IntStream.of(budgets).max().orElse(0);
+        // int max = 0;
+
+        // for (int b : budgets) max = Math.max(max, budgets[i]);
+
+        while (min <= max) {
+            final int mid = (min + max) / 2;
+            // final int mid = min + (max - min) / 2;
             
-            sum = 0;
-            for (int budget : budgets) {
-                if (budget >= mid) sum += mid;
-                else sum += budget;
-            }
+            int sum = IntStream.of(budgets).map(b -> Math.min(b, mid)).sum();
+            
+            /* int sum = 0;
+
+            for (int b : budgets) {
+                if (b > mid) sum += mid;
+                else sum += b;
+            } */
             
             if (sum <= M) {
+                min = mid + 1;
                 answer = Math.max(answer, mid);
-                low = mid + 1;
-            }
-            else {
-                high = mid - 1;
+            } else {
+                max = mid - 1;
             }
         }
         
-        return (int)answer;
+        return answer;
     }
 }
