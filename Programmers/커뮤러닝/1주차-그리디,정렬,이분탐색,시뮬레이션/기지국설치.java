6e73e22d540b7ee71dc5e96336430b4d5fa28009
dogfooter <yoon971224@gmail.com>
2022-02-21 12:29:53 +0900

    Update Code

--- Programmers/커뮤러닝/1주차-그리디,정렬,이분탐색,시뮬레이션/기지국설치.java
+++ Programmers/커뮤러닝/1주차-그리디,정렬,이분탐색,시뮬레이션/기지국설치.java
@@ -1,3 +1,8 @@
+/*  Tip. 효율성 높이기
+    1. Loop 개선
+    2. 적절한 데이터 구조 사용
+    3. 불필요한 객체 제거(Primitive Type 지양)  */
+    
 class Solution {
     public int solution(int n, int[] stations, int w) {
         int answer = 0;
