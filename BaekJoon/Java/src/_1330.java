edda36e68435ceac0e2459e77e2ec1a83d720fbe
dogfooter <yoon971224@gmail.com>
2022-02-23 21:18:52 +0900

    Update Code

--- BaekJoon/Java/src/_1330.java
+++ BaekJoon/Java/src/_1330.java
@@ -3,7 +3,7 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 import java.io.IOException;
 
-public class _1330 {
+class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
--- BaekJoon/Java/src/_18108.java
+++ BaekJoon/Java/src/_18108.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class _18108 {
+class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int year = sc.nextInt();
