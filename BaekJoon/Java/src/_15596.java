3a2b4376c69743a9623af97ff70bc01219ef81c7
dogfooter <yoon971224@gmail.com>
2022-02-24 23:28:12 +0900

    Update Code

---
+++ BaekJoon/Java/src/_15596.java
@@ -0,0 +1,7 @@
+public class _15596 {
+    long sum(int[] a) {
+        long answer = 0L;
+        for (int i = 0; i < a.length; ++i) answer += a[i];
+        return answer;
+    }
+}
---
+++ BaekJoon/Java/src/_2480.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+
+public class _2480 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in);
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st;
+        int T = Integer.parseInt(br.readLine());
+    }
+}
