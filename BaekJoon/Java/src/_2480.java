6b07e15a60fd1d3c5c5c032d7ef118985664046f
dogfooter <yoon971224@gmail.com>
2022-02-25 23:24:50 +0900

    Update Code

--- BaekJoon/Java/src/_2480.java
+++ BaekJoon/Java/src/_2480.java
@@ -1,9 +1,13 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+import java.util.StringTokenizer;
 
 public class _2480 {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         StringTokenizer st;
