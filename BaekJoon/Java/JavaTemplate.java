d043c2f5e1a75625f66e5c4dac37f6ad8849ae5d
dogfooter <yoon971224@gmail.com>
2022-02-25 23:24:59 +0900

    Update Code

--- BaekJoon/Java/JavaTemplate.java
+++ BaekJoon/Java/JavaTemplate.java
@@ -1,12 +1,15 @@
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
 import java.util.StringTokenizer;
 
 public class Solution {
     static int a, b, c;
 
     public static void main(String args[]) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         StringTokenizer st;
