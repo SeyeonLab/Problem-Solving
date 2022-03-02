e1608b8239c324f7ce476555a204f5f1a0e59472
dogfooter <yoon971224@gmail.com>
2022-03-02 16:19:25 +0900

    Update Code

---
+++ Programmers/연습_코딩테스트/DP/정수삼각형.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int solution(int[][] triangle) {
+        int answer = 0;
+        int size = triangle.length;
+        int[][] DP = new int[size + 1][size + 1];
+        
+        for (int i = 0; i < size; ++i) {
+            for (int j = 0; j <= i; ++j) {
+                if (i == 0) {
+                    DP[i][j] = triangle[i][j];
+                    break;
+                }
+                
+                if (j == 0) DP[i][j] = DP[i-1][j] + triangle[i][j];
+                else DP[i][j] = Math.max(DP[i-1][j-1], DP[i-1][j]) + triangle[i][j];
+            }
+        }
+        
+        for (int i = 0; i < size; ++i)
+            answer = Math.max(answer, DP[size - 1][i]);
+            
+        return answer;
+    }
+}
