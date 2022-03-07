6e0568f0df6796c9d847cc057f91c7323d96db33
dogfooter <yoon971224@gmail.com>
2022-03-07 18:14:33 +0900

    DP

--- Programmers/커뮤러닝/정수삼각형.java
+++ Programmers/커뮤러닝/정수삼각형.java
@@ -14,8 +14,7 @@ class Solution {
                 if (j == 0) DP[i][j] = DP[i-1][j] + triangle[i][j];
                 else DP[i][j] = Math.max(DP[i-1][j-1], DP[i-1][j]) + triangle[i][j];
             }
-        }
-        
+        }    
         for (int i = 0; i < size; ++i)
             answer = Math.max(answer, DP[size - 1][i]);
             
