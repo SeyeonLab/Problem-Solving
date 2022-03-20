d788c5c0f9392f20db7f70eedf9b306436e31bba
dogfooter <yoon971224@gmail.com>
2022-03-20 13:05:18 +0900

    Update Code

---
+++ Programmers/LEVEL 2/주식가격.java
@@ -0,0 +1,34 @@
+import java.util.Stack;
+
+class Solution {
+    public int[] solution(int[] prices) {
+        int[] answer = new int[prices.length];
+        Stack<Integer[]> stack = new Stack<>();
+        
+        for (int i = 0; i < prices.length; ++i) {
+            Integer[] temp = {prices[i], i};
+            
+            if (stack.empty()) {
+                stack.push(temp);
+                continue;
+            } else {
+                while (stack.peek()[0] > prices[i]) {
+                    answer[stack.peek()[1]] = i - stack.peek()[1];
+                    stack.pop();
+                    
+                    if (stack.empty())
+                        break;
+                }
+            }
+            
+            stack.push(temp);
+        }
+        
+        while (!stack.empty()) {
+            answer[stack.peek()[1]] = (prices.length-1) - stack.peek()[1];    
+            stack.pop();
+        }
+        
+        return answer;
+    }
+}
