2a0b4da0a7f6ff9da8b1dac40413f7c248e02fc4
dogfooter <yoon971224@gmail.com>
2022-03-21 23:14:05 +0900

    Update Code

--- BaekJoon/Java/src/_1000.java
+++ BaekJoon/Java/src/_1000.java
@@ -2,27 +2,19 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.io.IOException;
 import java.util.StringTokenizer;
+import java.io.IOException;
 
-public class _2480 {
+public class _1000 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        
+
         int a = Integer.parseInt(st.nextToken());
         int b = Integer.parseInt(st.nextToken());
-        int c = Integer.parseInt(st.nextToken());
-        
-        int answer = 0;
-        if (a == b && b == c) answer = 10000 + a * 1000;
-        else if (a == b || b == c) answer = 1000 + b * 100;
-        else if (a == c) answer = 1000 + a * 100;
-        else answer = Math.max(a, Math.max(b, c)) * 100;
-        
-        bw.write(String.valueOf(answer));
-        
+
+        bw.write(String.valueOf(a+b));
         bw.flush();
         bw.close();
     }
---
+++ BaekJoon/Java/src/_1008.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+import java.io.IOException;
+
+public class _1008 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        double answer = (double)a / b;
+        System.out.printf("%.32f", answer);
+    }
+}
