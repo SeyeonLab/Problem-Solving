2ff6cb3ae5a7934fcb6a9f9226292eb359144221
dogfooter <yoon971224@gmail.com>
2022-03-23 16:01:09 +0900

    Update Code

---
+++ BaekJoon/Java/src/_10952.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+import java.io.IOException;
+
+public class _10952 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+        while (true) {
+            try {
+                st = new StringTokenizer(br.readLine());
+                int A = Integer.parseInt(st.nextToken());
+                int B = Integer.parseInt(st.nextToken());
+                bw.write((A+B) + "\n");
+            }
+            catch (Exception e) {
+                break;
+            }
+        }
+
+        bw.flush();
+        bw.close();
+    }
+}
---
+++ BaekJoon/Java/src/_11021.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+import java.io.IOException;
+
+public class _11021 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int T = Integer.parseInt(br.readLine());
+        
+        for (int tc = 1; tc <= T; tc++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+            bw.write("Case #" + String.valueOf(tc) + ": ");
+            bw.write(String.valueOf(A+B) + "\n");
+        }
+
+        bw.flush();
+        bw.close();
+    }
+}
