17f3b53a25a248b74b8f4e2d3e16c61cd3b3c097
dogfooter <yoon971224@gmail.com>
2022-03-25 22:41:09 +0900

    Update Code

---
+++ BaekJoon/Java/src/_11654.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class _11654 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        char c = scanner.next().charAt(0);
+        System.out.print((int)c);
+        scanner.close();
+    }    
+}
