e99768da858306c8ef122c9e324f9476e21f69c6
dogfooter <yoon971224@gmail.com>
2022-03-29 04:19:27 +0900

    Update Code

---
+++ BaekJoon/Java/src/_13458.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+import java.io.IOException;
+
+public class _13458 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] examinee = new int[N];
+        
+        for (int i = 0; i < N; ++i) {
+
+        }
+        
+        int b = Integer.parseInt(st.nextToken());
+
+        bw.write(String.valueOf());
+        bw.flush();
+        bw.close();
+    }
+}
