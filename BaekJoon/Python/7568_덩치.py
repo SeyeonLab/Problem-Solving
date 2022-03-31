7aba5347b0e972766dc86be681e66c185edf8ade
dogfooter <yoon971224@gmail.com>
2022-03-31 16:29:15 +0900

    Update Code

--- BaekJoon/Python/7568_덩치.py
+++ BaekJoon/Python/7568_덩치.py
@@ -1,4 +1,16 @@
-N = 5
+N = int(input())
+weight = []
+height = []
 
 for i in range(N):
-    
+    x, y = map(int, input().split())
+    weight.append(x)
+    height.append(y)
+
+for i in range(N):
+    count = 1
+    for j in range(N):
+        if weight[i] < weight[j] and height[i] < height[j]:
+            count += 1
+
+    print(count, end=" ")
