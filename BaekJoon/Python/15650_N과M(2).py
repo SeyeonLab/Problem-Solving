38a861b70f1bb60a724b2f0d9b898e26a2b29215
dogfooter <yoon971224@gmail.com>
2022-04-21 00:12:38 +0900

    Update Code

--- BaekJoon/Python/15650_N과M(2).py
+++ BaekJoon/Python/15650_N과M(2).py
@@ -1,15 +1,15 @@
-def dfs():
+def dfs(start):
     if len(answer) == m:
         print(' '.join(map(str, answer)))
         return
 
-    for i in range(1, n+1):
+    for i in range(start, n+1):
         if i not in answer:
             answer.append(i)
-            dfs()
+            dfs(i+1)
             answer.pop()
 
 
 n, m = map(int, input().split())
 answer = []
-dfs()
+dfs(1)
