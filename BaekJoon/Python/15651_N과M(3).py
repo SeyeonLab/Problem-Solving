f846de6bdf4c88558bad542a3a781dacb2432615
dogfooter <yoon971224@gmail.com>
2022-04-23 08:39:55 +0900

    Update Code

---
+++ BaekJoon/Python/15651_N과M(3).py
@@ -0,0 +1,14 @@
+def dfs():
+    if len(answer) == m:
+        print(' '.join(map(str, answer)))
+        return
+
+    for i in range(1, n+1):
+        answer.append(i)
+        dfs()
+        answer.pop()
+
+
+n, m = map(int, input().split())
+answer = []
+dfs()
