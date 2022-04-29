fb5beb5f575b226b1a2b6d8c5ab96cbb0c377ca5
dogfooter <yoon971224@gmail.com>
2022-04-29 16:22:12 +0900

    Update Code

--- Programmers/연습_코딩테스트/Hash/완주하지못한선수.java
+++ Programmers/연습_코딩테스트/Hash/완주하지못한선수.java
@@ -6,20 +6,22 @@ class Solution {
     public String solution(String[] participant, String[] completion) {
         String answer = "";
         HashMap<String, Integer> hm = new HashMap<>();
-        
-        for (String member : participant) hm.put(member, hm.getOrDefault(member, 0) + 1);
-        for (String member : completion) hm.put(member, hm.get(member) - 1);
+
+        for (String member : participant)
+            hm.put(member, hm.getOrDefault(member, 0) + 1);
+        for (String member : completion)
+            hm.put(member, hm.get(member) - 1);
         Set<String> keys = hm.keySet();
         Iterator<String> it = keys.iterator();
-        
+
         while (it.hasNext()) {
             String key = it.next();
             int count = hm.get(key);
-            
+
             if (count > 0)
                 answer += key;
         }
-        
+
         return answer;
     }
 }
--- Programmers/연습_코딩테스트/Hash/전화번호목록.java
+++ Programmers/연습_코딩테스트/Hash/전화번호목록.java
@@ -3,19 +3,19 @@ import java.util.HashMap;
 class Solution {
     public boolean solution(String[] phone_book) {
         HashMap<String, Integer> map = new HashMap<>();
-        
+
         for (String phone : phone_book)
             map.put(phone, phone.length());
-        
+
         for (String phone : phone_book) {
-            for (int index = 0; index < phone.length(); index++){
+            for (int index = 0; index < phone.length(); index++) {
                 String temp = phone.substring(0, index);
                 System.out.println(temp);
                 if (map.containsKey(temp))
                     return false;
             }
         }
-        
+
         return true;
     }
 }
