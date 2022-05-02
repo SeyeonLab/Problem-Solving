ea228ae163748cc571295b44d32e7f25269720e0
dogfooter <yoon971224@gmail.com>
2022-05-02 19:26:56 +0900

    Update Code

---
+++ Programmers/문제모음/카카오 블라인드 채용 (2021)/신규아이디추천.py
@@ -0,0 +1,39 @@
+import re
+
+
+def solution(new_id):
+    answer = ''
+
+    # 1단계
+    new_id = new_id.lower()
+
+    # 2단계
+    for c in new_id:
+        if c.isalpha() or c.isdigit() or c in ['-', '_', '.']:
+            answer += c
+
+    # 3단계
+    while '..' in answer:
+        answer = answer.replace('..', '.')
+
+    # 4단계
+    if answer and answer[0] == '.':
+        answer = answer[1:]
+    if answer and answer[-1] == '.':
+        answer = answer[:-1]
+
+    # 5단계
+    if not answer:
+        answer += 'a'
+
+    # 6단계
+    if len(answer) >= 16:
+        answer = answer[:15]
+    if answer[-1] == '.':
+        answer = answer[:-1]
+
+    # 7단계
+    while len(answer) <= 2:
+        answer += answer[-1]
+
+    return answer
